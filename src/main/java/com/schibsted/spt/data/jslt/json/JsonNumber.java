adcfd82d70ba5ed96e4cc811f2323331d444fc5d
jarno.r <jrajala35@gmail.com>
2020-04-15 12:49:00 +0300

    Sync

--- src/main/java/com/schibsted/spt/data/jslt/json/JsonNumber.java
+++ src/main/java/com/schibsted/spt/data/jslt/json/JsonNumber.java
@@ -3,7 +3,14 @@ package com.schibsted.spt.data.jslt.json;
 /**
  * A class representing JSON number.
  *
- * This is the trickiest class. 
+ * JSON only defines a syntax for numbers, not semantics.
+ * JSON numbers are essentially arbitrary-precision decimal floating point numbers,
+ * but it might not be desirable to represent JSON numbers as java.math.BigDecimal for performance reasons.
+ * JSLT is agnostic to the number representation in Jackson NumberNode, but converts values to either long or double
+ * for numerical operations.
+ *
+ * For engineering tasks, double precision floating point is usually sufficient for all purposes.
+ *
  */
 public class JsonNumber extends JsonValue {
 }
--- src/main/java/com/schibsted/spt/data/jslt/json/JsonValue.java
+++ src/main/java/com/schibsted/spt/data/jslt/json/JsonValue.java
@@ -1,4 +1,24 @@
+/**
+ * Package for a minimalistic JSON data structure representation.
+ *
+ * Design principles:
+ * 1. Concrete classes with public constructors.
+ *    Pros: Avoid factories
+ *    Cons: Interfaces would allow different implementations depending on use-cases.
+ *
+ * 2. Immutability
+ *    Pros: The usual ones. Low-cost thread safety.
+ *    Cons: Potential performance issues. Additional complexity in manipulating arrays and objects.
+ *
+ * 3. Minimalism
+ *    Pros: Simplistic and concise design doesn't require frequent updates and retains backwards compatibility.
+ *    Cons: No helper methods in classes. Java doesn't have extension methods.
+ */
 package com.schibsted.spt.data.jslt.json;
 
-public class JsonValue {
+/**
+ * Base class for JSON values.
+ *
+ */
+public abstract class JsonValue {
 }
